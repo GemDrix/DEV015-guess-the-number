a9e48eb78f8eff12bd459fccc86c44da4b5b475c
Gema Riffo <gema.r.y382@gmai.com>
2024-10-04 10:45:51 -0300

    creación test unitarios y modificación para los test

---
+++ src/funciones_utiles.py
@@ -0,0 +1,27 @@
+"""Este módulo contiene funciones para realizar operaciones matemáticas ilogicas.
+"""
+def resultado_comparacion(consulta, numero):
+    """Compara un número consultado con un número objetivo.
+
+    Esta función verifica si el número consultado es mayor, menor o igual al número objetivo.
+    También valida que ambos números sean enteros.
+
+    Args:
+        consulta (int): El número que el usuario desea comparar.
+        numero (int): El número objetivo con el que se realiza la comparación.
+
+    Returns:
+        str: 
+            - "mayor" si la consulta es mayor que el número.
+            - "menor" si la consulta es menor que el número.
+            - "ganador" si la consulta es igual al número.
+            - "error" si alguno de los argumentos no es un entero.
+    """
+    if not isinstance(consulta, int) or not isinstance(numero, int):
+        return "error"
+    if consulta > numero:
+        return "mayor"
+    elif consulta < numero:
+        return "menor"
+    else:
+        return "ganador"
---
+++ src/jugador.py
@@ -0,0 +1,45 @@
+"""Este módulo contiene las funciones y clases básicas para el juego.
+"""
+import random
+
+
+class Jugador:
+    """Clase que representa a un jugador en el juego.
+
+    Esta clase almacena el nombre del jugador y un historial de sus elecciones durante el juego.
+
+    Atributos:
+        nombre (str): El nombre del jugador.
+        historial (list): Una lista que contiene el historial 
+        de elecciones, cada entrada es un diccionario que registra el turno y el número elegido.
+
+    Métodos:
+        __init__(nombre): Inicializa una nueva instancia de la 
+        clase Jugador con el nombre proporcionado.
+        registrar_eleccion(turno, eleccion): Registra la elección del jugador en su historial.
+    """
+
+    def __init__(self, nombre):
+        self.nombre = nombre
+        self.historial = []
+
+    def elegir_numero(self, min_n, max_n):
+        if self.nombre == "Computadora":
+            numero = random.randint(min_n, max_n)
+            print(f"{self.nombre} ha elegido el número {numero}")
+            return numero
+        else:
+            while True:
+                try:
+                    numero = int(
+                        input(f"{self.nombre}, ingresa un número entre {min_n} y {max_n}: "))
+                    if min_n <= numero <= max_n:
+                        return numero
+                    else:
+                        print(f"Por favor, ingresa un número válido entre {
+                              min_n} y {max_n}.")
+                except ValueError:
+                    print("Entrada no válida. Por favor, ingresa un número entero.")
+
+    def registrar_eleccion(self, turno, eleccion):
+        self.historial.append({"turno": turno, "elegido": eleccion})
